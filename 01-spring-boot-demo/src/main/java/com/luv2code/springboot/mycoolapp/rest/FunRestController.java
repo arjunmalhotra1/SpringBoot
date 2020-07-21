9e6bb206e73200afb7368b9040d29ad8bf14bf18
arjunmalhotra1 <am2cj@virginia.edu>
2020-07-21 15:52:54 -0500

    Spring-Boot-Project

---
+++ 01-spring-boot-demo/src/main/java/com/luv2code/springboot/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	//expose "/" endpoint that will return Hello World
+	
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "Hello World! Time on the server is "+ LocalDateTime.now();
+	}
+}
